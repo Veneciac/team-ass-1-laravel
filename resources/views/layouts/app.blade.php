f8d0830b5fd942b72a833d88e4dddee63f4784fb
Veneciac <veneciacalista00@gmail.com>
2022-06-26 00:22:25 +0700

    init react

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Team Assignment') }}</title>
+    <title>Team Assigment</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
